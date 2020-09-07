8ea7b7fd834a07593f378bfc885c437a67730112
Cornholius <y.layshkin@gmail.com>
2020-09-07 17:51:31 +0300

    сделал парс райфайзена

--- little_helper/main.py
+++ little_helper/main.py
@@ -1,5 +1,6 @@
-import config
 import logging
+import config
+from raiffeisen import Raiffeisen
 from aiogram import Bot, Dispatcher, executor, types
 
 
@@ -9,7 +10,7 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
-
+raif = Raiffeisen()
 
 # @dp.message_handler()
 # async def test(message: types.Message):
@@ -20,5 +21,40 @@ dp = Dispatcher(bot)
 async def test2(message: types.Message):
     await message.answer('ololo \n тест')
 
+@dp.message_handler(commands=['курс'])
+async def exchange(message: types.Message):
+    text = '''
+    Банк Райфайзен
+    
+    Курс валют в Онлайн кабинете:
+    Доллары:
+    покупка {}
+    продажа {}
+    Евро:
+    покупка {}
+    продажа {}
+    
+    Курс валют в Офисах банка:
+    Доллары:
+    покупка {}
+    продажа {}
+    Евро:
+    покупка {}
+    продажа {}
+    
+    Курс валют в банкоматах банка:
+    Доллары:
+    покупка {}
+    продажа {}
+    Евро:
+    покупка {}
+    продажа {}
+    '''.format(raif.online()['dollar_buy'], raif.online()['dollar_sell'],
+               raif.online()['euro_buy'], raif.online()['euro_sell'],
+               raif.office()['dollar_buy'], raif.online()['dollar_sell'],
+               raif.office()['euro_buy'], raif.online()['euro_sell'],
+               raif.card()['dollar_buy'], raif.online()['dollar_sell'],
+               raif.card()['euro_buy'], raif.online()['euro_sell'])
+    await message.answer(text)
 
 executor.start_polling(dp, skip_updates=True)
--- little_helper/raiffeisen.py
+++ little_helper/raiffeisen.py
@@ -1,26 +1,43 @@
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 import requests
 
 
-r = requests.get('https://www.raiffeisen.ru/currency_rates/?active_tab=offices')
-html = bs(r.content, 'html.parser')
+class Raiffeisen:
 
-# for i in html.select('.b-table__row'):
-#     if i.select('.desktop')[1].text == 'Доллар США':
-#         for q in i.select('.desktop')[1].text == 'Доллар США':
+    def __init__(self):
+        self.r = requests.get('https://www.raiffeisen.ru/currency_rates/?active_tab=offices')
+        self.html = BeautifulSoup(self.r.content, 'html.parser')
+        self.data = {'id_offices': 'offices', 'id_online': 'online', 'id_card': 'card',
+                     'us_str': 0, 'euro_str': 1,
+                     'offices_buy_value': 3, 'offices_sell_value': 5,
+                     'online_buy_value': 3, 'online_sell_value': 4,
+                     'card_buy_value': 1, 'card_sell_value': 2}
 
-# qwe = html.find("div", {"id": "offices"})
-# ewq = qwe.select('.b-table__row')[0]
-places = ["offices"]
-price = []
-for i in places:
-    dollar_buy = html.find("div", {"id": i}).select('.b-table__row')[0].select('.b-table__td')[3].text
-    dollar_sell = html.find("div", {"id": i}).select('.b-table__row')[0].select('.b-table__td')[5].text
-    euro_buy = html.find("div", {"id": i}).select('.b-table__row')[1].select('.b-table__td')[3].text
-    euro_sell = html.find("div", {"id": i}).select('.b-table__row')[1].select('.b-table__td')[5].text
-    data = {'dollar_buy': dollar_buy,
-            'dollar_sell': dollar_sell,
-            'euro_buy': euro_buy,
-            'euro_sell': euro_sell}
-    price.append(i)
-print(price)
+    def getprice(self, ids, us_str, euro_str, buy_value, sell_value):
+        usd = self.html.find("div", {"id": ids}).select('.b-table__row')[us_str]
+        euro = self.html.find("div", {"id": ids}).select('.b-table__row')[euro_str]
+        dollar_buy = usd.select('.b-table__td')[buy_value].text
+        dollar_sell = usd.select('.b-table__td')[sell_value].text
+        euro_buy = euro.select('.b-table__td')[buy_value].text
+        euro_sell = euro.select('.b-table__td')[sell_value].text
+        prices = {'dollar_buy': str(dollar_buy).replace(' ','').replace('\n', ''),
+                  'dollar_sell': str(dollar_sell).replace(' ','').replace('\n', ''),
+                  'euro_buy': str(euro_buy).replace(' ','').replace('\n', ''),
+                  'euro_sell': str(euro_sell).replace(' ','').replace('\n', '')}
+        return prices
+
+    def office(self):
+        return self.getprice(self.data['id_offices'], self.data['us_str'], self.data['euro_str'],
+                             self.data['offices_buy_value'], self.data['offices_sell_value'])
+
+    def online(self):
+        return self.getprice(self.data['id_online'], self.data['us_str'], self.data['euro_str'],
+                             self.data['online_buy_value'], self.data['online_sell_value'])
+
+    def card(self):
+        return self.getprice(self.data['id_card'], self.data['us_str'], self.data['euro_str'],
+                             self.data['card_buy_value'], self.data['card_sell_value'])
+
+
+qwe = Raiffeisen()
+print(qwe.card())
